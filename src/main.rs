d376b65b28e3f501cd526e8cafe1183c4cb35b47
Tanabebe <nob@tanabebe.tech>
2021-09-27 19:44:30 +0900

    CORS追加

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use actix_web::{get, HttpResponse, Result};
+use actix_web::{get, HttpResponse, Result, http::header};
 use serde::{Deserialize, Serialize};
+use actix_cors::Cors;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct User {
@@ -7,6 +8,7 @@ struct User {
     name: String,
     age: u32,
     personal_color: String,
+    hobbies: Vec<String>,
 }
 
 #[get("/users")]
@@ -17,18 +19,21 @@ async fn index() -> Result<HttpResponse> {
             name: String::from("主田"),
             age: 24,
             personal_color: String::from("blue"),
+            hobbies: vec![String::from(""), String::from("")],
         },
         User {
             id: 2,
             name: String::from("先岡"),
             age: 28,
             personal_color: String::from("pink"),
+            hobbies: vec![String::from("")],
         },
         User {
             id: 3,
             name: String::from("後藤"),
             age: 23,
             personal_color: String::from("green"),
+            hobbies: vec![String::from("game"), String::from("soccer")],
         },
     ];
     Ok(HttpResponse::Ok().json(user))
@@ -37,7 +42,18 @@ async fn index() -> Result<HttpResponse> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     use actix_web::{App, HttpServer};
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600);
+
+        App::new()
+            .wrap(cors)
+            .service(index)
+    })
         .bind("127.0.0.1:8080")?
         .run()
         .await
